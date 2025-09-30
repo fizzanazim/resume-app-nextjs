7fe726c6bfb888d182e562a59d72b631e37ab12c
fizzanazim <fizzanazim123@gmail.com>
2025-09-30 23:13:54 +0500

    initializing whole project

--- BackendwithMongodb/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { PostCvData, GetCvData, GetObjDataFunc } = require('../controllers/user.controller')
-const router = express.Router()
-
-router.post('/cv-data', PostCvData)
-// router.get('/receive-cv-data', GetCvData)
-router.get('/getobjdata/:id', GetObjDataFunc)
-
-module.exports = router
